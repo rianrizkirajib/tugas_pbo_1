2400c87da96e1166f9ab1b9fd84635c98616e296
rianrizkirajib <114301494+rianrizkirajib@users.noreply.github.com>
2022-09-25 20:43:52 +0700

    Add files via upload

---
+++ tugas_pbo_1.java
@@ -0,0 +1,18 @@
+package tugas_rianrizkirajib;
+import javax.swing.JOptionPane;
+
+// Rian Rizki Rajib TI21F
+
+public class tugas_pbo_1 {
+    public static void main(String[] args) {
+    // No. 1
+    JOptionPane.showMessageDialog(null, "Belajar Java");
+    JOptionPane.showMessageDialog(null, "Sangat Mudah Sekali");
+    JOptionPane.showMessageDialog(null, "dan Sangat Menyenangkan");
+    
+    // No. 2
+    String input = JOptionPane.showInputDialog("Matkul Apa Yang Anda Pelajari?");
+    JOptionPane.showMessageDialog(null, "Belajar " +input+ " sangat mudah.");           
+    }
+    
+}
